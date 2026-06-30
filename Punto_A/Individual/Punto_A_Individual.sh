a5798d69e692a6c6bd53dba817ce756aa30b1031
tsandovalsysadmin <tsandovalsysadmin@gmail.com>
2026-06-30 02:54:30 -0300

    PUNTO A — Estructura de Directorios: Sysadmin

---
+++ Punto_A/Individual/Punto_A_Individual.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+mkdir -p sysadmin/{Servicios/{Web,Base_de_Datos,Cache},Monitoreo/{Logs,Alertas,Metricas}}
+touch sysadmin/Servicios/{Web,Base_de_Datos,Cache}/.gitkeep
+touch sysadmin/Monitoreo/{Logs,Alertas,Metricas}/.gitkeep
